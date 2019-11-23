ea534ac777068320409c68a410137ddd06f22ede
rumd3x <edmurcardoso@gmail.com>
2019-11-23 00:45:29 -0300

    added manager base
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- manager/app.js
+++ manager/app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressLayouts = require('express-ejs-layouts')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+var apiRouter = require('./routes/api');
+var dashboardRouter = require('./routes/dashboard');
 
 var app = express();
 
@@ -15,20 +17,21 @@ app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(expressLayouts);
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', dashboardRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
