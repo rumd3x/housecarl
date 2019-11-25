ca5b1a70f4774d456036bdf9d04331223c94ca45
rumd3x <edmurcardoso@gmail.com>
2019-11-25 00:03:18 -0300

    added api method read sensors
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- manager/dao/readings.js
+++ manager/dao/readings.js
@@ -3,18 +3,20 @@ var mongoose = require('mongoose');
 const mongoIP = process.env.MONGODB_IP;
 const mongoPort = process.env.MONGODB_PORT;
 
-const connString = `mongodb://${mongoIP ? mongoIP : '127.0.0.1'}:${mongoPort ? mongoPort : '27017'}/housecarl-manager`;
+const connString = `mongodb://${mongoIP ? mongoIP : '172.17.0.1'}:${mongoPort ? mongoPort : '27017'}/housecarl-manager`;
 console.log("connString", connString);
 
 mongoose.connect(connString);
 
-var sensorsSchema = new mongoose.Schema({
+var sensorSchema = new mongoose.Schema({
     sensor: String,
     value: Object,
 }, { collection: "sensors" });
 
-const sensors = () => {
-    return mongoose.model('sensors', sensorsSchema, 'sensors');
+var sensors = mongoose.model('sensors', sensorSchema, 'sensors');
+
+var check = () => {
+    return mongoose.connection.db.databaseName === 'housecarl-manager';
 };
 
-module.exports = { sensors };
+module.exports = { sensors, check };
--- manager/routes/api.js
+++ manager/routes/api.js
@@ -1,9 +1,15 @@
+var dao = require('../dao/readings');
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/sensors', function (req, res, next) {
+
+  dao.sensors.find({}).lean().exec((e, docs) => {
+    res.send(docs);
+  });
+
 });
 
 module.exports = router;
