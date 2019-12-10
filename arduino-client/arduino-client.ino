bc92f623316175f3e4edcda0acc9d93b3a7ffd56
rumd3x <edmurcardoso@gmail.com>
2019-12-09 21:30:09 -0300

    client features and optimizations
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- arduino-client/arduino-client.ino
+++ arduino-client/arduino-client.ino
@@ -2,26 +2,30 @@
 #include <Ethernet.h>
 #include <SPI.h>
 
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-
-EthernetClient ethClient;
-HttpClient httpClient = HttpClient(ethClient, "192.168.1.3", 3000);
-
-const String loggerPath = "/api/logs";
-const String sensorStoragePath = "/api/sensors";
-
 #define LEVEL_DEBUG 0
 #define LEVEL_INFO 1
 #define LEVEL_WARN 2
 #define LEVEL_ERROR 3
 
-#define loggingLevel LEVEL_WARN
 #define serialPrintingEnabled false
 #define remoteLoggingEnabled true
 
 #define LM35 A0
 #define LDR 8
 #define PIR 9
+
+const String loggerPath = "/api/logs";
+const String sensorStoragePath = "/api/sensors";
+const String configurationPath = "/api/config/";
+
+const String loggingLevelSetting = "LOGGING_LEVEL";
+
+int loggingLevel = LEVEL_INFO;
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+EthernetClient ethClient;
+HttpClient httpClient = HttpClient(ethClient, "192.168.1.3", 3000);
 
 void (*resetBoard)(void) = 0;
 
@@ -31,26 +35,61 @@ void printSerial(String message, int level)
 
   if (level == LEVEL_INFO) {
     levelString = "INFO";
-  } else if (level == LEVEL_WARN) {
+  }
+  else if (level == LEVEL_WARN) {
     levelString = "WARN";
-  } else if (level >= LEVEL_ERROR) {
+  }
+  else if (level >= LEVEL_ERROR) {
     levelString = "ERROR";
   }
 
-  Serial.println("[" + levelString + "]: " + message);  
+  Serial.println("[" + levelString + "]: " + message);
+}
+
+String getCloudSetting(String setting, String def)
+{
+
+  httpClient.get(configurationPath + setting);
+
+  if (httpClient.responseStatusCode() < 0) {
+    resetBoard();
+  }
+
+  String response = httpClient.responseBody();
+
+  if (response == "unset") {
+    return def;
+  }
+
+  return response;
+}
+
+int makeRequest(String body, String path)
+{
+
+  httpClient.post(path, "application/json", body);
+
+  int responseCode = httpClient.responseStatusCode();
+  httpClient.responseBody();
+
+  if (responseCode < 0) {
+    resetBoard();
+  }
+
+  return responseCode;
 }
 
 void sendLogsToCloud(String message, int level)
 {
   String logObject = "{";
-  logObject += "\"message\":\""  + String(message) + "\"";
+  logObject += "\"message\":\"" + String(message) + "\"";
   logObject += ",\"level\":" + String(level);
   logObject += "}";
 
   makeRequest(logObject, loggerPath);
 }
 
-void logMessage(String message, int level = LEVEL_INFO)
+void logMessage(String message, int level)
 {
 
   if (level < loggingLevel) {
@@ -72,26 +111,11 @@ void hang(String message)
   while (true);
 }
 
-int makeRequest(String body, String path)
-{
-  
-  httpClient.post(path, "application/json", body);
-  
-  int responseCode = httpClient.responseStatusCode();
-  httpClient.responseBody();
-
-  if (responseCode < 0) {
-    resetBoard();
-  }
-  
-  return responseCode;
-}
-
 bool sendSensorReadings(String readings)
-{  
+{
   logMessage("f=sendSensorReadings;message=Starting request", LEVEL_INFO);
   int responseCode = makeRequest(readings, sensorStoragePath);
-  
+
   logMessage("f=sendSensorReadings;responseCode=" + String(responseCode), LEVEL_INFO);
   return responseCode == 200;
 }
@@ -104,10 +128,8 @@ float readLM35()
   int samples[iterations];
   int combo = 0;
 
-  for (int i = 0; i < iterations; i++)
-  {
-    do
-    {
+  for (int i = 0; i < iterations; i++) {
+    do {
       samples[i] = analogRead(LM35);
     } while (samples[i] <= 20);
 
@@ -125,29 +147,38 @@ float readTemperature()
 {
   float temperature = 0;
 
-  for (int i = 0; i < 100; i++)
-  {
+  for (int i = 0; i < 100; i++) {
     temperature += readLM35();
     delay(5);
   }
 
   temperature = temperature / 100.0;
   logMessage("f=readTemperature;temperature=" + String(temperature), LEVEL_DEBUG);
-    
+
   return temperature;
 }
 
+bool lastPirReading = false;
+bool readMovement()
+{
+  bool currentReading = (digitalRead(PIR) == HIGH);
+  bool constantlyMoving = (currentReading && lastPirReading);
+  lastPirReading = currentReading;
+
+  return constantlyMoving;
+}
+
 String readSensors()
-{  
-  
+{
+
   logMessage("f=readSensors;message=Starting sensor readings", LEVEL_DEBUG);
-  
+
   String encodedReadings = "{";
   encodedReadings += "\"room_temperature\":" + String(readTemperature());
   encodedReadings += ",\"room_luminosity\":" + String(digitalRead(LDR) == LOW);
-  encodedReadings += ",\"room_movement\":" + String(digitalRead(PIR) == HIGH);
+  encodedReadings += ",\"room_movement\":" + String(readMovement());
   encodedReadings += "}";
-  
+
   return encodedReadings;
 }
 
@@ -157,22 +188,20 @@ void setup()
   pinMode(LDR, INPUT);
   pinMode(PIR, INPUT);
 
-  if (serialPrintingEnabled)
-  {
+  if (serialPrintingEnabled) {
     Serial.begin(9600);
     while (!Serial);
+    Serial.println("serial ok");
   }
 
   Ethernet.begin(mac);
   delay(1000);
 
-  if (Ethernet.hardwareStatus() == EthernetNoHardware)
-  {
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
     hang("EthNoHW");
   }
 
-  if (Ethernet.linkStatus() == LinkOFF)
-  {
+  if (Ethernet.linkStatus() == LinkOFF) {
     hang("LOFF");
   }
 
@@ -180,23 +209,28 @@ void setup()
 }
 
 void loop()
-{
+{  
+  loggingLevel = getCloudSetting(loggingLevelSetting, String(LEVEL_INFO)).toInt();
+  logMessage("f=loop;message=Set logging level: " + String(loggingLevel), LEVEL_DEBUG);
+  
   int attempt = 1;
   String encodedReadings = readSensors();
   bool sentReadingsSuccessfully = false;
 
-  // int sleepMs = 3000;
-
   do {
-      
-    logMessage("f=loop;message=Attempt " + String(attempt), LEVEL_WARN);
+
+    int attemptLevel = LEVEL_INFO;
+    if (attempt > 1) {
+      attemptLevel = LEVEL_WARN;
+    }
+    logMessage("f=loop;message=Attempt " + String(attempt), attemptLevel);
+
     sentReadingsSuccessfully = sendSensorReadings(encodedReadings);
-      
     attempt++;
-    // sleepMs -= 200;
-    
+
   } while (!sentReadingsSuccessfully && attempt <= 10);
-  
-  logMessage("f=loop;message=Waiting", LEVEL_DEBUG);
-  // delay(sleepMs);
+
+  if (!sentReadingsSuccessfully) {
+    logMessage("f=loop;message=Failed to send readings", LEVEL_ERROR);
+  }
 }
